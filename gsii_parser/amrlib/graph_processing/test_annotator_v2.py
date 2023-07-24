d4c181004092f887ef7941179a02ac0b6aef035c
atlasfox007 <13519109@std.stei.itb.ac.id>
2023-07-24 07:50:04 +0700

    feat: Complete feature annotator

--- gsii_parser/amrlib/graph_processing/test_annotator_v2.py
+++ gsii_parser/amrlib/graph_processing/test_annotator_v2.py
@@ -1,21 +1,29 @@
 import torch
 from transformers import BertTokenizer, BertForTokenClassification
+from nlp_id.tokenizer import Tokenizer
+from nlp_id.postag import PosTag
+from nlp_id.lemmatizer import Lemmatizer 
 
+####################################################################
 # Indonesia Tokenizer
-from nlp_id.tokenizer import Tokenizer 
-id_tokenizer = Tokenizer() 
 
-# BERT Tokenizer and NER Tagger
-tokenizer = BertTokenizer.from_pretrained("cahya/bert-base-indonesian-NER")
+id_tokenizer = Tokenizer()
+
+input_words = "Presiden Indonesia yang terakhir adalah Joko Widodo !"
+input_words = id_tokenizer.tokenize(input_words)
+print(input_words)
+
+####################################################################
+# NER TAGGER
+
+# BERT NER Tagger
+ner_tokenizer = BertTokenizer.from_pretrained("cahya/bert-base-indonesian-NER")
 ner = BertForTokenClassification.from_pretrained("cahya/bert-base-indonesian-NER")
 
 # Input sentence (array of words)
-input_words = "Presiden Indonesia yang terakhir adalah Joko Widodo !"
-input_words = id_tokenizer.tokenize(input_words)
 
-print(input_words)
 # Tokenize the input sentence and get attention_mask
-tokenized_input = tokenizer(" ".join(input_words), add_special_tokens=False, return_tensors="pt", padding=True, truncation=True, max_length=512)
+tokenized_input = ner_tokenizer(" ".join(input_words), add_special_tokens=False, return_tensors="pt", padding=True, truncation=True, max_length=512)
 input_ids = tokenized_input["input_ids"]
 attention_mask = tokenized_input["attention_mask"]
 
@@ -28,6 +36,19 @@ active_logits = logits[attention_mask == 1] # Consider only non-padding tokens
 flattened_predictions = torch.argmax(active_logits, axis=1)
 id2label = ner.config.id2label
 
-token_predictions =[id2label[i] for i in flattened_predictions.numpy()]
-print(input_words)
-print(token_predictions)
+ner_token_predictions =[id2label[i] for i in flattened_predictions.numpy()]
+print(ner_token_predictions)
+####################################################################
+
+####################################################################
+# POS TAGGER
+postagger = PosTag()
+pos_tag = postagger.get_pos_tag(" ".join(input_words))
+pos_tag_result = [x[1] for x in pos_tag]
+print(pos_tag_result)
+####################################################################
+# LEMMATIZER
+lemmatizer = Lemmatizer() 
+lemma_result = lemmatizer.lemmatize(" ".join(input_words))
+print(lemma_result.split())
+####################################################################
