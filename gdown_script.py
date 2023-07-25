00ff1c4c4ee3623cbffc50bb2fc4c8ea9df1f3d8
atlasfox007 <13519109@std.stei.itb.ac.id>
2023-07-26 01:20:04 +0700

    misc:runpod related files

---
+++ gdown_script.py
@@ -0,0 +1,11 @@
+import gdown
+
+# bert-base-cased
+gdown.download_folder("https://drive.google.com/drive/folders/1A4Jgo3BhlcmK4jfaPZbQQd0GCNFtCixO?usp=drive_link")
+
+# data
+gdown.download_folder("https://drive.google.com/drive/folders/1wdVhNm_5KLoCrGHd8AE98qCQT5zZfGkd?usp=drive_link")
+
+# pre_trained_model
+
+gdown.download_folder("https://drive.google.com/drive/folders/1gEDt05tlmlxd5i2bDDPegxs7fETEvEHC?usp=sharing")
