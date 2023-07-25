41b295e28c96f73b99edd36ec1278dfe2c1f7c59
chrslex <alexandro.tobing@gmail.com>
2023-07-25 19:59:07 +0000

    Add utility files

--- gdown_script.py
+++ gdown_script.py
@@ -1,11 +1,10 @@
 import gdown
 
 # bert-base-cased
-gdown.download_folder("https://drive.google.com/drive/folders/1A4Jgo3BhlcmK4jfaPZbQQd0GCNFtCixO?usp=drive_link")
+# gdown.download_folder("https://drive.google.com/drive/folders/1A4Jgo3BhlcmK4jfaPZbQQd0GCNFtCixO?usp=drive_link")
 
-# data
-gdown.download_folder("https://drive.google.com/drive/folders/1wdVhNm_5KLoCrGHd8AE98qCQT5zZfGkd?usp=drive_link")
-
+# data.zip
+gdown.download("https://drive.google.com/uc?id=1YTymgnKSfl2_Kfn9dXryTewxGIRA_YfI", "data.zip")
 # pre_trained_model
 
-gdown.download_folder("https://drive.google.com/drive/folders/1gEDt05tlmlxd5i2bDDPegxs7fETEvEHC?usp=sharing")
+# gdown.download_folder("https://drive.google.com/drive/folders/1gEDt05tlmlxd5i2bDDPegxs7fETEvEHC?usp=sharing")
