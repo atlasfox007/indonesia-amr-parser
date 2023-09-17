1167c31447ff203d0a41a5cbeb346369348a19cd
christian.tobing <christian.tobing@munalively.com>
2023-09-17 13:32:59 +0700

    delete demo folder

--- demo/generate_single_sentence.sh
+++
@@ -1,6 +0,0 @@
-# Define the path to the Python interpreter and the script
-PYTHON_EXEC="/home/chrslex/miniconda3/envs/amr/bin/python"
-SCRIPT_PATH="/mnt/d/indonesia-amr-parser/gsii_parser/scripts/30_Model_Parse_GSII/31_Generate_Sentence.py"
-
-# Execute the Python script
-$PYTHON_EXEC $SCRIPT_PATH
